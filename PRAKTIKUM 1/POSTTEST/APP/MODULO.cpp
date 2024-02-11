9f42423994e547edeed610330b5f2f637a1a20ca
Naufal Anas <92364025+naufalanasea@users.noreply.github.com>
2024-02-11 15:28:13 +0700

    Add files via upload

---
+++ PRAKTIKUM 1/POSTTEST/APP/MODULO.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+ 
+int main(void)
+{
+     
+    // Untuk memasukkan dua nilai integer
+    int x, y;
+ 
+    // Untuk menginput hasil dari modulus
+    int hasil;
+ 	
+    x = 3;
+    y = 6;
+    hasil = x % y;
+    cout << hasil << endl;
+ 
+    x = 4;
+    y = 5;
+    hasil = x % y;
+    cout << hasil << endl;
+ 
+    x = 5;
+    y = 4;
+    hasil = x % y;
+    cout << hasil ;
+ 
+    return 0;
+}
